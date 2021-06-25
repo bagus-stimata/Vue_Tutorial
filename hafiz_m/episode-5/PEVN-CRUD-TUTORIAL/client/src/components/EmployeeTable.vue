152f47eace0f35a23e6011de4517f91c8f4fb510
bagus winarno <bagus.stimata@gmail.com>
2021-06-25 23:02:22 +0700

    oke

---
+++ hafiz_m/episode-5/PEVN-CRUD-TUTORIAL/client/src/components/EmployeeTable.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <div>
+        <v-text-field></v-text-field>
+    </div>
+    <div>
+        <v-text-field></v-text-field>
+    </div>
+    <div>
+        <v-text-field></v-text-field>
+    </div>
+</div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
