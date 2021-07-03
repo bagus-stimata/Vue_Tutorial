b1c998ba4e1764105b1a5526ff68b42174acdfb8
bagus winarno <bagus.stimata@gmail.com>
2021-07-03 07:56:02 +0700

    oke tahu caranya vuex

--- Vuex_Tutorial/vue2-vuex-counter/src/store/index.js
+++ Vuex_Tutorial/vue2-vuex-counter/src/store/index.js
@@ -5,13 +5,36 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    counter: 100,
-    colorCode: 'red'
+    counter: 0,
+    currentJoke: 'tes bos',
+    allJokes: []
   },
   mutations: {
+    increaseCounter(state){
+      state.counter++
+    },
+    decreaseCounter(state){
+      state.counter--
+    },
+    setCurrentJoke(state, payload){
+        state.currentJoke = payload
+        state.allJokes.push(payload)
+    }
   },
   actions: {
+    increaseCounterDispatch(){
+      this.commit('increaseCounter')
+    },
+    decreaseCounterDispatch(){
+      this.commit('decreaseCounter')
+    }
+
   },
   modules: {
+  },
+  getters: {
+    getCurrentJoke(state){
+      return state.currentJoke
+    }
   }
 })
--- Vuex_Tutorial/vue2-vuex-counter/src/views/About.vue
+++ Vuex_Tutorial/vue2-vuex-counter/src/views/About.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <h2>{{ $store.state.counter}} </h2>
+    <!-- <h1>This is an about page</h1> -->
+    <h1>{{ $store.state.counter}} </h1>
+    <h1> Hello {{ joke }}</h1>
+
     <v-row>
       <v-col>
         <v-btn
@@ -13,16 +15,42 @@
 
       </v-col>
     </v-row>
+
+    <div class="buttons">
+      <v-btn @click="$store.commit('increaseCounter')">Plus</v-btn>
+      <v-btn @click="$store.commit('decreaseCounter')">Minus</v-btn>
+    </div>
+    <div class="buttons">
+      <v-btn @click="$store.dispatch('increaseCounterDispatch')">Plus</v-btn>
+      <v-btn @click="$store.dispatch('decreaseCounterDispatch')">Minus</v-btn>
+    </div>
+    <div class="buttons">
+      <v-btn @click="increaseCounterMethod">Plus</v-btn>
+      <v-btn @click="decreaseCounterMethod">Minus</v-btn>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+      return {
+        joke: 'Ada saja'
+      }
+  },
+  mounted(){
+    this.$store.mutations.setCurrentJoke('aku')
+    this.joke = this.$store.getters.currentJoke
+  },
   methods: {
-    test() {
-      $store.mutations.
-      console.log("Hello World");
+    increaseCounterMethod() {
+      this.$store.commit('increaseCounter')
+    },
+    decreaseCounterMethod() {
+      this.$store.dispatch('decreaseCounterDispatch')
     }
+
   }
   
 }
